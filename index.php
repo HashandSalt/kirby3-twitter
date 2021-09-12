352a401c7812f7df684870e8c4a021d4c250502e
Martin Folkers <hello@twobrain.io>
2021-09-12 22:32:37 +0200

    Taming the chaos (#4)
    * Combine snippets
    
    * Indent & annotate code
    
    * Update README
    
    * Add missing annotations & type hints

--- index.php
+++ index.php
@@ -3,27 +3,26 @@
 @include_once __DIR__ . '/vendor/autoload.php';
 
 load([
-    'hashandsalt\\twitter\\twitter' => 'src/classes/Twitter.php'
+    'hashandsalt\\twitter\\twitter' => 'src/classes/Twitter.php',
 ], __DIR__);
 
 use HashAndSalt\Twitter\Twitter;
 
+
 Kirby::plugin('hashandsalt/twitter', [
-
+    # Snippets
     'snippets' => [
-        'twitter/tweet'     => __DIR__ . '/snippets/tweet.php',
-        'twitter/tweets'    => __DIR__ . '/snippets/tweets.php'
-      ],
-  
-    'pageMethods' => [
-        'tweets' => function ($type, $count, $er, $screenname) {
-            $init = new Twitter();
-            return $init->timeline($type, $count, $er, $screenname);
-        },
-        'tweet' => function ($id) {
-            $init = new Twitter();
-            return $init->single($id);
-        }
-    ]
+        'twitter/tweet'  => __DIR__ . '/snippets/tweet.php',
+        'twitter/tweets' => __DIR__ . '/snippets/tweets.php',
+    ],
 
+    # Page methods
+    'pageMethods' => [
+        'tweets' => function (string $type, int $count, bool $excludeReplies, string $screenName) {
+            return (new Twitter())->timeline($type, $count, $excludeReplies, $screenName);
+        },
+        'tweet' => function (string $id) {
+            return (new Twitter())->single($id);
+        },
+    ],
 ]);
--- snippets/tweets.php
+++ snippets/tweets.php
@@ -1,22 +1,7 @@
-<?php $tweets = $page->tweets($type, $count, $excludereplies, $screenname); ?>
-
 <ul class="tweetlist">
-    <?php foreach ($tweets as $tweet): ?>
-
-        <li>
-        
-            <?= $tweet['full_text'] ?>
-        
-            <?php
-            if($media === true) {
-                if(A::get($tweet, 'entities.media')) {
-                $url = $tweet['entities']['media'][0]['media_url_https'];
-                echo Html::img($url);
-                }
-            }
-            ?>  
-        
-        </li>
-
-    <?php endforeach; ?>
+    <?php foreach ($page->tweets($type, $count, $excludereplies, $screenname) as $tweet) : ?>
+    <li>
+        <?php snippet('twitter/tweet', compact('tweet')) ?>
+    </li>
+    <?php endforeach ?>
 </ul>
