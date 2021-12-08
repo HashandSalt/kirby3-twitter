b24a31fd5302b9b00a3f696b33b77774f3c66c87
Hash&Salt <hello@hashandsalt.com>
2021-12-08 10:04:11 +0000

    Typo fixe

--- snippets/tweets.php
+++ snippets/tweets.php
@@ -1,7 +1,7 @@
 <?php $tweets = $page->tweets($type, $count, $excludeReplies, $screenName); ?>
 
 <ul class="tweetlist">
-    <?php foreach ($page->tweets($type, $count, $excludereplies, $screenname) as $tweet) : ?>
+    <?php foreach ($page->tweets($type, $count, $excludeReplies, $screenName) as $tweet) : ?>
     <li>
         <?php snippet('twitter/tweet', compact('tweet')) ?>
     </li>
